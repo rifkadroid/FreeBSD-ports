00a12724ddc2aa85bbb09cb3c83a08d45906c62b
KONNTROL Tecnologia <guzzy@uol.com.br>
2021-03-24 17:26:03 -0300

    Update e2guardian.widget.php

--- www/Kontrol-pkg-E2guardian5/files/usr/local/www/widgets/widgets/e2guardian.widget.php
+++ www/Kontrol-pkg-E2guardian5/files/usr/local/www/widgets/widgets/e2guardian.widget.php
@@ -79,7 +79,7 @@ echo"  </tr>";
 echo"</table></div>";
 
 ?>
-<script src="/vendor/jquery/jquery-1.12.0.min.js" type="text/javascript"></script>
+<script src="/vendor/jquery/jquery-3.5.1.min.js" type="text/javascript"></script>
 <script type="text/javascript">
    function getstatus_e2guardian() {
 	var url = "/widgets/widgets/e2guardian.widget.php";
